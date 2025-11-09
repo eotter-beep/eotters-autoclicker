134617c937cc135740bba1addc281c5e456f1073
eotter-beep <eotter76@outlook.com>
2025-11-09 17:35:00 +0000

    Decrease sleep time in autoclicker script
    Reduced sleep duration for faster clicking.

--- autoclicker.sh
+++ autoclicker.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Simple autoclicker using xdotool
-# Press Ctrl+C to stop
-
-echo "Starting autoclicker... (Ctrl+C to stop)"
-
 while true; do
   xdotool click 1   # left-click
-  sleep 0.1
+  sleep 0.001
 done
